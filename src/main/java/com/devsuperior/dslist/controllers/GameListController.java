f8c7c62695482c4a8a161efc92de5b590fb3d5d2
Larissa Ferreira de Jesus <larissafj@gmail.com>
2025-01-21 11:14:36 -0300

    Finalização aula 2

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.PrivilegedAction;
 import java.util.List;
 
 @RestController
@@ -19,10 +20,21 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
 
     @GetMapping
     public List<GameListDTO> findAll(){
         List<GameListDTO> result = gameListService.findAll();
         return result;
+    }
+
+    @GetMapping( value = "/{listId}/games")
+    public List<GameMinDTO>findByList(@PathVariable Long listId){
+        List<GameMinDTO> result = gameService.findByList(listId);
+        return result;
+
+
     }
 }
--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.service;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repository.GameRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,12 @@ public class GameService {
             List<Game> result = gameRepository.findAll();
             List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
             return dto;
+    }
+
+    @Transactional
+    public List<GameMinDTO> findByList(Long listId){
+       List<GameMinProjection> result = gameRepository.searchByList(listId);
+       return result.stream().map(x -> new GameMinDTO(x)).toList();
+
     }
 }
