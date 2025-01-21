89599d6440520bc9c3d13e65951c50830a3b54ba
Larissa Ferreira de Jesus <larissafj@gmail.com>
2025-01-21 10:08:29 -0300

    Domain Model

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -30,6 +30,7 @@ public class Game {
     public  Game(){
 
     }
+
     public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
